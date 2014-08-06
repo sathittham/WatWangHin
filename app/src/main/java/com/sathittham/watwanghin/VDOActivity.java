db619ca46c80f8bbe3b4d9bcd8166f9ee50c3280
sathittham <s.sathittham@gmail.com>
2014-08-07 00:06:00 +0700

    vdo player work

--- app/src/main/java/com/sathittham/watwanghin/VDOActivity.java
+++ app/src/main/java/com/sathittham/watwanghin/VDOActivity.java
@@ -3,110 +3,76 @@ package com.sathittham.watwanghin;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.content.Intent;
+import android.content.Intent;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayer.ErrorReason;
-import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
-import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
-import com.google.android.youtube.player.YouTubePlayer.Provider;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+
+public class VDOActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
 
 
-public class VDOActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+    // public static final String API_KEY = "AIzaSyD4v5-ipnGWTw7QZe6LYSh31cFA-ow6grA";
 
-    public static final String API_KEY = "AIzaSyD4v5-ipnGWTw7QZe6LYSh31cFA-ow6grA";
+    private YouTubePlayer YPlayer;
+    private static final String YoutubeDeveloperKey = "AIzaSyD4v5-ipnGWTw7QZe6LYSh31cFA-ow6grA";
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
 
-    //http://youtu.be/<VIDEO_ID>
-    public static final String VIDEO_ID = "dKLftgvYsVU";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_vdo);
-
-        /** Initializing YouTube player view **/
-        YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
-        youTubePlayerView.initialize(API_KEY, this);
-	}
     @Override
-    public void onInitializationFailure(Provider provider, YouTubeInitializationResult result) {
-        Toast.makeText(this, "Failured to Initialize!", Toast.LENGTH_LONG).show();
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_vdo);
+
+        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(YoutubeDeveloperKey, this);
+
+    }
+
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    "There was an error initializing the YouTubePlayer",
+                    errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
-    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(YoutubeDeveloperKey, this);
+        }
+    }
 
-        /** add listeners to YouTubePlayer instance **/
-        player.setPlayerStateChangeListener(playerStateChangeListener);
-        player.setPlaybackEventListener(playbackEventListener);
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
 
-        /** Start buffering **/
+    @Override
+    public void onInitializationSuccess(Provider provider,
+                                        YouTubePlayer player, boolean wasRestored) {
         if (!wasRestored) {
-            player.cueVideo(VIDEO_ID);
+            YPlayer = player;
+            /*
+             * Now that this variable YPlayer is global you can access it
+             * throughout the activity, and perform all the player actions like
+             * play, pause and seeking to a position by code.
+             */
+            YPlayer.cueVideo("2zNSgSzhBfM");
         }
     }
 
-    private PlaybackEventListener playbackEventListener = new PlaybackEventListener() {
-
-        @Override
-        public void onBuffering(boolean arg0) {
-
-        }
-
-        @Override
-        public void onPaused() {
-
-        }
-
-        @Override
-        public void onPlaying() {
-
-        }
-
-        @Override
-        public void onSeekTo(int arg0) {
-
-        }
-
-        @Override
-        public void onStopped() {
-
-        }
-
-    };
-
-    private PlayerStateChangeListener playerStateChangeListener = new PlayerStateChangeListener() {
-
-        @Override
-        public void onAdStarted() {
-
-        }
-
-        @Override
-        public void onError(ErrorReason arg0) {
-
-        }
-
-        @Override
-        public void onLoaded(String arg0) {
-
-        }
-
-        @Override
-        public void onLoading() {
-        }
-
-        @Override
-        public void onVideoEnded() {
-
-        }
-
-        @Override
-        public void onVideoStarted() {
-
-        }
-    };
 }
